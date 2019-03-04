4c9f8476ab478b2e2a45e8ab852eb0c48572b4d2
Parker Steichen <parker7s@uw.edu>
2019-03-03 17:16:20 -0800

    updated to PEP8 and improved functions

--- Explore_Dev/shoyu.py
+++ Explore_Dev/shoyu.py
@@ -8,9 +8,9 @@ pickle file.
 Developed by Raman-Noodles team.
 """
 
-import jcamp
 import os
 import pickle
+import jcamp
 import requests
 
 
@@ -19,31 +19,36 @@ def download_cas(cas_num):
     Function that saves raman data
     downloaded from NIST webpage.
     """
-    if os.path.exists('raman_spectra/'+cas_num+'_NIST_IR.jdx') == False:
+    # drop any '-' from cas_num
+    cas_num = ''.join(cas_num.split('-'))
+    if not os.path.exists('raman_spectra/'+cas_num+'_NIST_IR.jdx'):
         os.makedirs('raman_spectra', exist_ok=True)
         url = 'https://webbook.nist.gov/cgi/cbook.cgi?JCAMP=C'+cas_num+'&Index=1&Type=IR'
-        r = requests.get(url, allow_redirects=True)
-        if r.text == '##TITLE=Spectrum not found.\n##END=\n':
+        data = requests.get(url, allow_redirects=True)
+        if data.text == '##TITLE=Spectrum not found.\n##END=\n':
             print("""Bad Request: CAS # not found. Please verify
              with NIST Chemistry WebBook.""")
         else:
-            if r.status_code == 200:
-                open('raman_spectra/'+cas_num+'_NIST_IR.jdx', 'wb').write(r.content)
+            if data.status_code == 200:
+                open('raman_spectra/'+cas_num+'_NIST_IR.jdx', 'wb').write(data.content)
                 print('file downloaded too raman_spectra folder')
             else:
-                print('Request status: {}'.format(r.status_code))
+                print('Request status: {}'.format(data.status_code))
     else:
         print('file already in raman_spectra folder')
 
 
-def add_jdx(filename, label):
+def add_jdx(filename, label=None):
     """
     Function that reads and adds a .jdx file to the
     raman_data_dict pickle file.
     """
     shoyu_data_dict = pickle.load(open('raman_spectra/shoyu_data_dict.p', 'rb'))
     data = jcamp.JCAMP_reader(filename)
-    shoyu_data_dict.update({label: data})
+    if label is None:
+        shoyu_data_dict.update({data['title']: data})
+    else:
+        shoyu_data_dict.update({label: data})
     pickle.dump(shoyu_data_dict, open('raman_spectra/shoyu_data_dict.p', 'wb'))
     print('shoyu_data_dict.p updated')
     return shoyu_data_dict
@@ -55,6 +60,8 @@ def more_please(cas_num, label):
     database, adds it to shoyu_data_dict, pickles shoyu_data_dict
     and returns the updated shoyu_data_dict.
     """
+    # Drop any '-' from cas_num
+    cas_num = ''.join(cas_num.split('-'))
     download_cas(cas_num)
     shoyu_data_dict = add_jdx('raman_spectra/'+cas_num+'_NIST_IR.jdx', label)
     return shoyu_data_dict
