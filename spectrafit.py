780be0217b34b1635f74cca332941fe2c4696d8c
Parker Steichen <parker7s@uw.edu>
2019-03-07 23:05:35 -0800

    completed baseline_subtraction function and unit test

--- spectrafit.py
+++ spectrafit.py
@@ -8,23 +8,27 @@ from peakutils.baseline import baseline
 import matplotlib.pyplot as plt
 
 
-def subtract_baseline(y_data, deg=5, plot=False):
+def subtract_baseline(y_data, deg=5, plot=False, x_data=None):
     """
     Function that fits a n-degree polynomial (default: n=5)
     baseline and subtracts it from the input data
     """
-    yb = baseline(y_data, deg=deg, max_it=200)
+    y_base = baseline(y_data, deg=deg, max_it=200)
     # to avoid strange results,
     # change all negative values to zero
-    yb_plus = [0 if i < 0 else i for i in yb]
+    yb_plus = [0 if i < 0 else i for i in y_base]
     y_out = y_data - yb_plus
-    
-    if plot:
+    # plot that lets you see the baseline fitting
+    if plot and x_data is None:
+        print('Please add x_data as input to plot')
+    elif plot:
         plt.figure(figsize=(10,4))
-        plt.plot(x, y_data, 'b--', label='original')
-        plt.plot(x, y_out, 'b', label='output')
-        plt.plot(x, yb_plus, 'r', label='baseline')
-        plt.plot(x, yb, 'r--', label='negative baseline')
+        plt.plot(x_data, y_data, 'b--', label='input')
+        plt.plot(x_data, y_out, 'b', label='output')
+        plt.plot(x_data, yb_plus, 'r', label='baseline')
+        plt.plot(x_data, y_base, 'r--', label='negative baseline')
         plt.axhline(y=0, color='orange', alpha=0.5, label='zero') 
         plt.legend()
+    else:
+        pass
     return y_out
---
+++ test_spectrafit.py
@@ -0,0 +1,36 @@
+"""
+This is the unit test module for spectrafit.py
+"""
+
+import numpy as np
+import spectrafit
+
+
+# define gaussian function used to generate test data (fake spectra)
+def gaussian(x_input, var_a, var_b, var_c):
+    """Simple function to produce a gaussian distribution"""
+    y_output = var_a*np.exp(-((x_input-var_b)**2)/(2*(var_c**2)))
+    return y_output
+
+
+# generate test data
+X_TEST = np.arange(500, 3500, 5)
+# generate 6 gaussians
+gauss1 = gaussian(X_TEST, 0.9, 2000, 100)
+gauss2 = gaussian(X_TEST, 0.5, 1000, 200)
+gauss3 = gaussian(X_TEST, 0.4, 2500, 1000)
+gauss4 = gaussian(X_TEST, 0.7, 3200, 50)
+gauss5 = gaussian(X_TEST, 0.7, 2200, 20)
+gauss6 = gaussian(X_TEST, 0.7, 4000, 500)
+# add gaussians to create fake spectra
+Y_TEST = gauss1 + gauss2 + gauss3 + gauss4 + gauss5 + gauss6
+# normalize test spectra
+Y_TEST = [(Y_TEST[i] - min(Y_TEST))/(max(Y_TEST)-min(Y_TEST)) for i in range(len(Y_TEST))]
+Y_TEST = np.asarray(Y_TEST)
+
+
+def test_subtract_baseline():
+    """docstring"""
+    y_data = spectrafit.subtract_baseline(X_TEST, Y_TEST)
+    assert isinstance(y_data, numpy.array), 'output is not a numpy array'
+    assert len(y_data) == len(Y_TEST), 'output length different from input'
