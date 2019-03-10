57815ebba3d3248b6e83f13c5b98cb1c877b92f2
Brandon Kern <kernb2@uw.edu>
2019-03-09 23:08:35 -0800

    functioning code

--- ramannoodles/h2o.py
+++ ramannoodles/h2o.py
@@ -10,27 +10,32 @@ from ramannoodles import spectrafit
 import pickle
 
 shoyu.initialize_standard_library()
-# import data dict
+# open spectra library
 shoyu_data_dict = pickle.load(open('../raman_spectra/shoyu_data_dict.p', 'rb'))
+# list keys
+sorted(shoyu_data_dict.keys())
 
 def h2o():
     data = shoyu_data_dict['WATER']
     x_data = data['x']
     y_data = data['y']
+
+    data_range = max(x_data) - min(x_data)
+    data_range
     xmin = data['x'].min()
     xmax = data['x'].max()
     xrange=xmax-xmin
-    # # subtract baseline
-    # y_data = spectrafit.subtract_baseline(y_data, deg=3, plot=True, x_data=x_data)
-    #print(xmin,xmax,xrange)
+    # subtract baseline
+    y_data = spectrafit.subtract_baseline(y_data, deg=3, plot=True, x_data=x_data)
     # detect peaks
-    peaks = spectrafit.find_peaks(x_data, y_data)
+    # detect peaks
+    peaks = spectrafit.find_peaks(x_data, y_data, thres=0.25, min_dist=50)
     # assign parameters for least squares fit
     mod, pars = spectrafit.lorentz_params(peaks)
     # fit the model to the data
     out = spectrafit.model_fit(x_data, y_data, mod, pars, report=True)
     # plot fit results
-    plot = spectrafit.plot_fit(x_data, y_data, out, plot_components=True)
+    spectrafit.plot_fit(x_data, y_data, out, plot_components=True)
     # export data in logical structure (see docstring)
     fit_peak_data = spectrafit.export_fit_data(out)
     # print docstring which outlines data structure
