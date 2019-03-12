f7b37ce3c23a6597694efb81d2928cb0313e69b8
Parker Steichen <parker7s@uw.edu>
2019-03-12 13:52:25 -0700

    added basic unit tests for all functions

--- ramannoodles/tests/test_shoyu.py
+++ ramannoodles/tests/test_shoyu.py
@@ -2,38 +2,45 @@
 Tests for functions in the shoyu.py module
 """
 
+import os
+import pickle
 from ramannoodles import shoyu
 
+# open spectra library
+shoyu_data_dict = pickle.load(open('../raman_spectra/shoyu_data_dict.p', 'rb'))
 
 def test_download_cas():
     """docstring"""
     # CAS registry number for water
     cas_num = '7732-18-5'
     shoyu.download_cas(cas_num)
-    assert os.path.isdir('.../raman_spectra/')
+    assert os.path.isdir('../raman_spectra/'), 'Directory not found'
     
     
-# def test_add_jdx():
-#     """docstring"""
-#     # i need a test jdx?
+def test_add_jdx():
+    """docstring"""
+    filename = '7732185_NIST_IR.jdx'
+    shoyu.add_jdx('../raman_spectra/'+filename)
+    assert os.path.isfile('../raman_spectra/'+filename), 'File not found'
 
     
-# def test_initialize_standard_library():
-#     """docstring"""
-#     shoyu.initialize_standard_library()
+def test_initialize_standard_library():
+    """docstring"""
+    shoyu.initialize_standard_library()
+    assert os.path.isfile('../raman_spectra/shoyu_data_dict.p'), 'File not found'
     
-    
-# def test_more_please():
-#     """docstring"""
-#     # CAS registry number for water
-#     cas_num = '7732-18-5'
-#     shoyu.more_please(cas_num)
+def test_more_please():
+    """docstring"""
+    # CAS registry number for pentane
+    cas_num = '109-66-0'
+    shoyu.more_please(cas_num)
+    assert os.path.isfile('../raman_spectra/109660_NIST_IR.jdx'), 'File not found' 
     
     
 def test_combine_spectra():
     """docstring"""
-    compound_1 = 'WATER'
-    compound_2 = 'CARBON MONOXIDE'
+    compound_1 = shoyu_data_dict['WATER']
+    compound_2 = shoyu_data_dict['CARBON MONOXIDE']
     data = shoyu.combine_spectra(compound_1, compound_2)
     assert len(data[0]) == len(data[1])
     
