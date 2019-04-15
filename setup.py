a2dc51113b86c4b99c3bb66401bab65155dba935
Parker Steichen <parker7s@uw.edu>
2019-04-14 21:24:09 -0700

    updated version number to 2.0.beta

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ramannoodles',
-      version='1.0',
+      version='2.0.beta',
       description='A python package for analyzing raman spectroscopy data.',
       url='https://github.com/raman-noodles/Raman-noodles',
       author='Raman Noodles Group, University of Washington (2019)',
